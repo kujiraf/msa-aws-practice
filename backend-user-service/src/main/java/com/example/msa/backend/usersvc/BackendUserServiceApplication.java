1a0d0fdd700903ae7798b0ddb7221659d2ace087
root <frkw123@gmail.com>
2022-05-26 23:17:27 +0900

    backend initial commit

---
+++ backend-user-service/src/main/java/com/example/msa/backend/usersvc/BackendUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.msa.backend.usersvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendUserServiceApplication.class, args);
+	}
+
+}
