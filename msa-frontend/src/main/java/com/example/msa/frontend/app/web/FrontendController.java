527a9e962b372fbf862d2ad5618013c416fa09d2
root <frkw123@gmail.com>
2022-05-26 10:20:23 +0900

    login成功時の画面遷移まで完了

--- msa-frontend/src/main/java/com/example/msa/frontend/app/web/FrontendController.java
+++ msa-frontend/src/main/java/com/example/msa/frontend/app/web/FrontendController.java
@@ -1,7 +1,11 @@
 package com.example.msa.frontend.app.web;
 
+import javax.servlet.http.HttpSession;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import com.example.msa.frontend.app.web.security.CustomUserDetails;
 
 @Controller
 public class FrontendController {
@@ -19,5 +23,19 @@ public class FrontendController {
   @GetMapping(value = "/timeout")
   public String timeout() {
     return "timeout";
+  }
+
+  @GetMapping(value = "/portal")
+  public String portal(
+      @AuthenticationPrincipal CustomUserDetails customUserDetails,
+      Model model,
+      HttpSession httpSession) {
+    //    model.addAttribute(
+    //        "portalInformation",
+    //
+    // PortalInformation.builder().userResource(customUserDetails.getUserResource()).build());
+    String sessionId = httpSession.getId();
+    model.addAttribute("sessionId", sessionId);
+    return "portal";
   }
 }
