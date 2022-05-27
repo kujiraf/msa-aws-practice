c8500f8203af656ae19264bcc76b2e55ae3f19bb
root <frkw123@gmail.com>
2022-05-27 09:09:56 +0900

    common initial commit

---
+++ common/src/test/java/com/example/msa/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msa.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
