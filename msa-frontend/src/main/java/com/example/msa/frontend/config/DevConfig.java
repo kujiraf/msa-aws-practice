b818ded688d6cbd8eeeab2a85f333fba9efc0e31
root <frkw123@gmail.com>
2022-05-29 14:29:30 +0900

    impl conf

---
+++ msa-frontend/src/main/java/com/example/msa/frontend/config/DevConfig.java
@@ -0,0 +1,17 @@
+package com.example.msa.frontend.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.reactive.function.client.WebClient;
+import com.example.msa.frontend.domain.ServiceProperties;
+
+@Profile("dev")
+@Configuration
+public class DevConfig {
+  @Autowired ServiceProperties serviceProperties;
+
+  public WebClient userWebClient() {
+    return WebClient.builder().baseUrl(serviceProperties.getDns()).build();
+  }
+}
